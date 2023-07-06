9d4d74585018bcba45003ed1c9dafba939466e81
boyoun_park <boyounpark2019@gmail.com>
2023-07-06 13:49:11 +0900

    [refactor] changed uncalled functions into private field

--- TDD_StringSimilarity/checker.cpp
+++ TDD_StringSimilarity/checker.cpp
@@ -8,31 +8,6 @@ public:
 		: answer(str) {
 	}
 
-	int getTotalCnt(int alphaCntInput[26], int alphaCntAnswer[26])
-	{
-		int totalCnt = 0;
-		for (int letter = 0; letter < 26; ++letter){
-			if (alphaCntInput[letter] || alphaCntAnswer[letter]) totalCnt++;
-		}
-		return totalCnt;
-	}
-
-	int getSameCnt(int alphaCntInput[26], int alphaCntAnswer[26])
-	{
-		int sameCnt = 0;
-		for (int letter = 0; letter < 26; ++letter){
-			if (alphaCntInput[letter] && alphaCntAnswer[letter]) sameCnt++;
-		}
-		return sameCnt;
-	}
-
-	void countEachAlpha(const string& str, int alphaCnt[26])
-	{
-		for (char letter : str) {
-			alphaCnt[letter - 'A']++;
-		}
-	}
-
 	int getAlphaScore(const string& str) {
 		int alphaCntInput[26] = { 0 };
 		int alphaCntAnswer[26] = { 0 };
@@ -45,6 +20,32 @@ public:
 
 		return (sameCnt * 40)/totalCnt;
 	}
+
 private:
+	int getTotalCnt(int alphaCntInput[26], int alphaCntAnswer[26])
+	{
+		int totalCnt = 0;
+		for (int letter = 0; letter < 26; ++letter) {
+			if (alphaCntInput[letter] || alphaCntAnswer[letter]) totalCnt++;
+		}
+		return totalCnt;
+	}
+
+	int getSameCnt(int alphaCntInput[26], int alphaCntAnswer[26])
+	{
+		int sameCnt = 0;
+		for (int letter = 0; letter < 26; ++letter) {
+			if (alphaCntInput[letter] && alphaCntAnswer[letter]) sameCnt++;
+		}
+		return sameCnt;
+	}
+
+	void countEachAlpha(const string& str, int alphaCnt[26])
+	{
+		for (char letter : str) {
+			alphaCnt[letter - 'A']++;
+		}
+	}
+
 	string answer;
 };
