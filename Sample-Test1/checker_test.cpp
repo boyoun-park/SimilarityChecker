da4fc3292066fd0cf41442d10a9967572efcaf12
boyoun_park <boyounpark2019@gmail.com>
2023-07-06 13:39:14 +0900

    [feat] add partial alpha score

--- Sample-Test1/checker_test.cpp
+++ Sample-Test1/checker_test.cpp
@@ -2,8 +2,15 @@
 #include "../TDD_StringSimilarity/checker.cpp"
 
 TEST(SimilarityChecker, sameAlphabetAll) {
-	SimilarityChecker machine;
+	SimilarityChecker machine("DSA");
 	int expected = 40;
-	int actual = machine.getAlphaScore("DSA");
+	int actual = machine.getAlphaScore("ASD");
+	EXPECT_EQ(expected, actual);
+}
+
+TEST(SimilarityChecker, sameAlphabetPartial) {
+	SimilarityChecker machine("AAE");
+	int expected = 20;
+	int actual = machine.getAlphaScore("AA");
 	EXPECT_EQ(expected, actual);
 }
--- TDD_StringSimilarity/checker.cpp
+++ TDD_StringSimilarity/checker.cpp
@@ -4,7 +4,31 @@ using namespace std;
 
 class SimilarityChecker {
 public:
-	int getAlphaScore(const string& str) {
-		return 40;
+	explicit SimilarityChecker(const string& str)
+		: answer(str) {
 	}
+
+	int getAlphaScore(const string& str) {
+		int totalCnt = 0;
+		int sameCnt = 0;
+		int alphaCntInput[26] = {0};
+		int alphaCntAnswer[26] = { 0 };
+
+		for (int i = 0; i < str.length(); i++) {
+			alphaCntInput[str[i] - 'A']++;
+		}
+		for (int j = 0; j < answer.length(); j++) {
+			alphaCntAnswer[answer[j] - 'A']++;
+		}
+
+		for (int i = 0; i < 26; ++i){
+			if (alphaCntInput[i] && alphaCntAnswer[i]) sameCnt++;
+			else if (alphaCntInput[i] || alphaCntAnswer[i]) totalCnt++;
+		}
+		totalCnt += sameCnt;
+
+		return sameCnt*40/totalCnt;
+	}
+private:
+	string answer;
 };
